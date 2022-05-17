f771d0334905366c056a8eddd6aa42d184103e00
Isaac Franco <isaac@Isaacs-Mac-Pro.local>
2022-05-17 01:32:38 -0300

    security fixes

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
  
 const cors = require('cors');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 const morgan = require('morgan');
  
 const app = express();
  
 app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
  
-app.use(helmet());
+//app.use(helmet());
  
 app.use(express.json());
  
 app.use(morgan('dev'));
+
+app.use(express.static('public'));
  
 app.post('/login', (req, res, next) => {
     res.json({ token: '123456' });
