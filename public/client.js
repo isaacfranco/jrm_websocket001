91239a851d14f27c658deeb9ccfa52cb9d346f26
Isaac Franco <isaacfranco@gmail.com>
2022-05-17 01:47:53 -0300

    indent

--- public/client.js
+++ public/client.js
@@ -1,30 +1,27 @@
 const ws = new WebSocket("ws://" + location.host);
-        let msg;
-        let chat;
+let msg;
+let chat;
 
-        ws.onmessage = (event) => {        
-            console.log(event.data);
-            const json = JSON.parse(event.data);
-            console.log('json', json);
-            if (json.type == 'broadcast') {
-                chat.innerText += `${json.message}\n`;
-            }
-        }
+ws.onmessage = (event) => {        
+    console.log(event.data);
+    const json = JSON.parse(event.data);
+    console.log('json', json);
+    if (json.type == 'broadcast') {
+        chat.innerText += `${json.message}\n`;
+    }
+}
 
-        function send() {
-            
-            ws.send(JSON.stringify({
-                type: 'message', 
-                message: msg.value
-            }));
-        }
+function send() {
+    
+    ws.send(JSON.stringify({
+        type: 'message', 
+        message: msg.value
+    }));
+}
 
-        window.addEventListener('load', (e) => {
-            console.log('load')
-            msg = document.getElementById('message');
-            chat = document.getElementById('chat');
-<<<<<<< HEAD:public/client.js
-        });
-=======
-        });
->>>>>>> f771d0334905366c056a8eddd6aa42d184103e00:cliente.html
+window.addEventListener('load', (e) => {
+    console.log('load')
+    msg = document.getElementById('message');
+    chat = document.getElementById('chat');
+});
+
